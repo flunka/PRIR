ffcc80496d62c7e10c6deba6ca81d99c6898728c
Piotr <flunka@gmail.com>
2018-04-11 08:44:38 +0200

    Init commit

---
+++ primes_omp.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <omp.h>
+
+using namespace std;
+
+#define PRECISION 100
+
+int main( int argc, char** argv )
+{
+    
+
+    return 0;
+}
